eefa7efbdd7b76f5574950719c27634e8cd15ad7
owokomoses <owokomose@gmail.com>
2024-05-14 09:59:02 +0300

    added userRoutes

---
+++ routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const userController = require('../controller/userController');
+const isAdmin = require('../middleware/isAdmin');
+const router = express.Router();
+
+router.post('/addUser',  userController.addUser);
+router.get("/getAllUsers", userController.getAllUsers);
+router.put("/updateUser/:id",  userController.updateUser);
+router.delete("/deleteUser/:id", userController.deleteUser);
+router.post('/loginUser', userController.loginUser);
+router.post('/registerAdmin', userController.registerAdmin);
+router.post('/loginAdmin', userController.loginAdmin);
+
+module.exports = router;
