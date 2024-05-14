8300a4ccd8babaa88aaaf176f4a84729690ae5b0
owokomoses <owokomose@gmail.com>
2024-05-14 18:27:21 +0300

    added dashboardModel

---
+++ routes/dashboardRoutes.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+// const dashboardController = require('../controller/dashboardController');
+// const router = express.Router();
+
+// router.get('/', dashboardController.getAllItems);
+// router.get('/:id', dashboardController.getItemById);
+// router.post('/', dashboardController.createItem);
+// router.put('/:id', dashboardController.updateItem);
+// router.delete('/:id', dashboardController.deleteItem);
+
+// module.exports = router;
