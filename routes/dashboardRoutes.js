73974ad77f4418e6a10b92b8625bb6cb8b160736
owokomoses <owokomose@gmail.com>
2024-05-14 18:28:43 +0300

    added dashboardRoutes

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,11 +1,11 @@
-// const express = require('express');
-// const dashboardController = require('../controller/dashboardController');
-// const router = express.Router();
+const express = require('express');
+const dashboardController = require('../controller/dashboardController');
+const router = express.Router();
 
-// router.get('/', dashboardController.getAllItems);
-// router.get('/:id', dashboardController.getItemById);
-// router.post('/', dashboardController.createItem);
-// router.put('/:id', dashboardController.updateItem);
-// router.delete('/:id', dashboardController.deleteItem);
+router.get('/', dashboardController.getAllItems);
+router.get('/:id', dashboardController.getItemById);
+router.post('/', dashboardController.createItem);
+router.put('/:id', dashboardController.updateItem);
+router.delete('/:id', dashboardController.deleteItem);
 
-// module.exports = router;
+module.exports = router;
