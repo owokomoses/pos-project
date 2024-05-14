90a443d896ba23259186b986f274d22184e31f23
owokomoses <owokomose@gmail.com>
2024-05-14 09:56:23 +0300

    added userModel.js

---
+++ model/userModel.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt');
+module.exports=(sequelize, DataTypes) =>{
+    const user = sequelize.define('users', {
+
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },  
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },  
+        role: {
+            type: DataTypes.ENUM('user', 'admin'),
+            allowNull: false,
+            defaultValue: 'user'
+        }      
+    });
+
+     // Before validating the user, convert email to lowercase
+user.beforeValidate((user, options) => {
+    if (user.email) {
+    user.email = user.email.toLowerCase();
+    }
+});
+
+    // Before creating a new user, hash the password
+user.beforeCreate(async (user) => {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+});
+
+  //   Compare the incoming password with the stored password
+    user.prototype.isValidPassword = async function (password) {
+        try {
+            return  await bcrypt.compare(password, this.password);
+        } catch (error) {
+            throw  error;
+        }
+    };
+    return user;
+}
